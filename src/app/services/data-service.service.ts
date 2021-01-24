846aeb2a921c8e12c08b1afd9f1882cef51faa5a
Jonathas Germano <jonathas.germano@hotmail.com>
2021-01-23 22:12:50 -0300

    Project title modified

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -8,7 +8,7 @@ import { GlobalDataSummary } from '../models/global-data';
 })
 export class DataService {
 
-  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-03-2020.csv`
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-20-2021.csv`
   constructor(private http: HttpClient) { }
 
   getGlobalData() {
